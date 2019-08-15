562a9ded67c77c32405756bd515afeb03beb7d14
dhiratjoe <dtjoe@student.unimelb.edu.au>
2019-08-15 22:40:21 +1000

    Added 'homepage' + route to homepage

--- app.js
+++ app.js
@@ -4,10 +4,18 @@ var app = express();
 var bodyParser = require('body-parser');
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true }));
+const path = require('path');
+const router = express.Router();
+
+router.get('/',function(req,res){
+  res.sendFile(path.join(__dirname+'/views/index.html'));
+  //__dirname : It will resolve to your project folder.
+});
 
 // Database setup
 require('./models/db.js');
 
+app.use('/', router);
 // Start the server
 app.listen(3000,function(req,res){
    console.log('Express listening on port 3000');
